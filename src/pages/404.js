63ad3de8441136ed5523cabcc5dfc7bd9c68d39c
ericmlyons <edub187@yahoo.com>
2021-11-06 10:15:11 -0400

    Initial Commit

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,28 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Client from "../components/client";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
-const NotFoundPage = () => {
-  return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
-
+const NotFoundPage = () => (
+  <Layout>
+    <SEO title="404: Not found" />
+    <Client />
+    <div className="error-page-wrapper">
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-12">
+                    <div className="inner">
+                        <h1 className="theme-color">404</h1>
+                        <h2>Page Not Found</h2>
+                        <p>Sorry, but the page you are looking for was moved, removed, renamed or might never existed...</p>
+                        <a className="rn-button" href="/">Go Back</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  </Layout>
+)
 export default NotFoundPage
+
+
